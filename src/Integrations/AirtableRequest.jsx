059dfe9d0b021860804a13c5a19bc4bddebe2cde
dev-angelo-c <jcrish@gmail.com>
2023-12-23 13:27:33 -0500

    update pacakge and react counter for fetch request duplication for no reason

--- src/Integrations/AirtableRequest.jsx
+++ src/Integrations/AirtableRequest.jsx
@@ -5,23 +5,25 @@ import airtableYoutubeLinks from "../main";
 
 export default () => {
   let [results, setResults] = useState([]);
-  let [beginFetch, setBeginFetch] = useState(false)
+  let [beginFetch, setBeginFetch] = useState(0)
 
   useEffect( () => {
-    setBeginFetch(true);
-    const fetchAirtable = async () => {
-      try{
-        const airtableResponse = await airtableYoutubeLinks()
-        console.log(" Do you do this? ", airtableResponse?.data?.records.length);
-        setResults(airtableResponse?.data);
-      }catch(e){
-        console.error(" Error Fetching Airtable links");
+    setBeginFetch(beginFetch++);
+    if(beginFetch < 1){
+      const fetchAirtable = async () => {
+        try{
+          const airtableResponse = await airtableYoutubeLinks()
+          console.log(" Do you do this? ", airtableResponse?.data?.records.length);
+          setResults(airtableResponse?.data);
+        }catch(e){
+          console.error(" Error Fetching Airtable links");
+        }
       }
+      
+      fetchAirtable();
     }
-    
-    fetchAirtable();
     return () => {
-      beginFetch(false);
+      setBeginFetch(0);
       console.log(" Everything should be done now ", console.log(" results ", results));
     }
   }, [])
